44aff6525e0007f46cdaa07a2c3f43413a4ee6f5
KiriRainCat <2287990465@qq.com>
2023-06-24 14:38:48 +0800

    📃 docs: 测试描述汉化

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,18 +3,18 @@
 import i18n from '../../src/i18n'
 import { langs } from '../../src/i18n'
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
+describe('E2E（端到端）测试示例', () => {
+  it('访问应用根目录', () => {
     cy.visit('/')
     cy.title().should('eq', i18n.global.t('messages.appName'))
   })
 
-  it('redirects back to home if 404', () => {
+  it('如果 404，重定向至主页', () => {
     cy.visit('/someUnknownUrl')
     cy.location('href').should('not.include', '/someUnknownUrl')
   })
 
-  it('switches languages successfully', () => {
+  it('语言切换组件正常工作', () => {
     cy.visit('/')
 
     langs.forEach((lang) => {
