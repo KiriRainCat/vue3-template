c2db1710f52562e1d7854b845e96525887b709f7
KiriRainCat <2287990465@qq.com>
2023-07-28 20:08:12 +0800

    ✨ feat: 注释掉Element Plus的自动引入解析器

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // ==== Plugins for Auto Import ==== //
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import Icons from 'unplugin-icons/vite'
 import IconResolver from 'unplugin-icons/resolver'
@@ -23,11 +23,12 @@ export default defineConfig({
         globalsPropValue: true,
       },
       imports: ['vue', 'pinia', 'vue-router', 'vue-i18n'],
-      resolvers: [ElementPlusResolver()],
+      // resolvers: [ElementPlusResolver()],
     }),
     Components({
       dts: './src/components.d.ts',
-      resolvers: [ElementPlusResolver(), IconResolver()],
+      resolvers: [IconResolver()],
+      // resolvers: [ElementPlusResolver(), IconResolver()],
     }),
     Icons({ autoInstall: true, compiler: 'vue3' }),
     vue(),
